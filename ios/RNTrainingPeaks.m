4492114c92b056a26c443903c422755077428496
valeri <valeritsert@gmail.com>
2019-04-04 19:07:09 +0400

    readFitFile

--- ios/RNTrainingPeaks.m
+++ ios/RNTrainingPeaks.m
@@ -9,5 +9,19 @@
 }
 RCT_EXPORT_MODULE()
 
+RCT_EXPORT_METHOD(readFitFile: (NSString *) path
+                  resolver:(RCTPromiseResolveBlock)resolve
+                  rejecter:(RCTPromiseRejectBlock)reject) {
+  NSURL *fileUrl = [NSURL fileURLWithPath:path];
+  NSData *fileData = [NSData dataWithContentsOfURL:fileUrl];
+  if (fileData != NULL) {
+   
+    NSString *str = [fileData base64EncodedStringWithOptions:0];
+    resolve(str)
+  } else {
+    reject(@"file_not_found");
+  }
+}
+  
 @end
   
