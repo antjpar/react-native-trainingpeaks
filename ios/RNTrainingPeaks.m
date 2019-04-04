4ffe9b5b0081bd1a75ae891fc0b455959e019d3e
valeri <valeritsert@gmail.com>
2019-04-04 20:27:42 +0400

    login

--- ios/RNTrainingPeaks.m
+++ ios/RNTrainingPeaks.m
@@ -1,3 +1,5 @@
+#import <SafariServices/SafariServices.h>
+#import <Foundation/Foundation.h>
 
 #import "RNTrainingPeaks.h"
 
@@ -9,6 +11,57 @@
 }
 RCT_EXPORT_MODULE()
 
+
+  // client_id, redirect_uri, response_type = 'code', approval_prompt = 'auto', scope = 'activity:write,read'
+  RCT_EXPORT_METHOD(login:(NSString*)client_id
+                    redirect_uri:(NSString*)redirect_uri
+                    response_type:(NSString*)response_type
+                    scope:(NSString*)scope
+                    sandbox: (BOOL) sandbox
+                    ) {
+    NSString* sandboxUrl = @"https://oauth.sandbox.trainingpeaks.com/OAuth/Authorize";
+    NSString* productionUrl = @"https://oauth.trainingpeaks.com/OAuth/Authorize";
+    
+    response_type = response_type == NULL ? @"code" : response_type;
+    scope = scope == NULL ? @"events:write,events:read,athlete:profilese" : scope;
+    NSString* url = sandbox ? sandboxUrl : productionUrl;
+    
+    
+    NSString* endpoint = [NSString stringWithFormat: @"%@?client_id=%@&redirect_uri=%@&response_type=%@&scope=%@", url, client_id, redirect_uri, response_type, scope ];
+
+    NSURL* authUrl = [NSURL URLWithString:endpoint];
+    
+    if ([UIApplication.sharedApplication canOpenURL:authUrl ]) {
+      [UIApplication.sharedApplication openURL:authUrl];
+    } else {
+      if (@available(iOS 11.0, *)) {
+        SFAuthenticationSession* _authSession = [[SFAuthenticationSession alloc] initWithURL:authUrl callbackURLScheme:@"noblepro://" completionHandler:^(NSURL * _Nullable callbackURL, NSError * _Nullable error) {
+          [[UIApplication sharedApplication] openURL:callbackURL];
+        }];
+        [_authSession start];
+      }
+      
+      //    if (@available(iOS 12.0, *)) {
+      //
+      //      _authSessionA = [[ASWebAuthenticationSession alloc] initWithURL: webOAuthUrl callbackURLScheme:@"noblepro://mobile" completionHandler:^(NSURL * _Nullable callbackURL, NSError * _Nullable error) {
+      //        [[UIApplication sharedApplication] openURL:callbackURL];
+      //      }];
+      //      [_authSessionA start];
+      //    } else {
+      //      if (@available(iOS 11.0, *)) {
+      //        _authSession = [[SFAuthenticationSession alloc] initWithURL:webOAuthUrl callbackURLScheme:@"noblepro://" completionHandler:^(NSURL * _Nullable callbackURL, NSError * _Nullable error) {
+      //          [[UIApplication sharedApplication] openURL:callbackURL];
+      //        }];
+      //        [_authSession start];
+      //      } else {
+      //        // Fallback on earlier versions
+      //      }
+      //    }
+    }
+  }
+
+  
+  
 RCT_EXPORT_METHOD(readFitFile: (NSString *) path
                   resolver:(RCTPromiseResolveBlock)resolve
                   rejecter:(RCTPromiseRejectBlock)reject) {
